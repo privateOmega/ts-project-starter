af07b639f61239f75ec85a15a2c8d1327b2f154c
privateOmega <privateOmega@protonmail.com>
2023-02-04 19:05:10 +0530

    chore: update eslint config to lint with type information

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,15 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
+  ],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['./tsconfig.json'],
+    tsconfigRootDir: __dirname,
+  },
   plugins: ['@typescript-eslint'],
   root: true,
   ignorePatterns: 'dist/**/*.js',
